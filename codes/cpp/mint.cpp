1869071f1ca9e55d022b88ad6f9bb2f33e1f23bd
kjnh10 <kojinho10@gmail.com>
2019-10-15 23:20:52 +0900

    Add bsgs

---
+++ codes/cpp/mint.cpp
@@ -0,0 +1,45 @@
+//%snippet.set('mint')%
+int mod = 1e9+7;
+struct mint { //{{{
+  int x;
+  mint(int x=0):x((x%mod+mod)%mod){}
+
+  // ?= operator
+  mint& operator+=(const mint a) { (x += a.x) %= mod; return *this; }
+  mint& operator-=(const mint a) { (x += mod-a.x) %= mod; return *this; }
+  mint& operator*=(const mint a) { (x *= a.x) %= mod; return *this; }
+  mint&operator/=(const mint&rhs){return *this*=rhs.inv();}
+
+  mint operator+(const mint a) const { mint res(*this); return res+=a; }
+  mint operator-(const mint a) const { mint res(*this); return res-=a; }
+  mint operator*(const mint a) const { mint res(*this); return res*=a; }
+  mint operator/(const mint&rhs)const{return mint(*this)/=rhs;}
+
+  mint pow(int n)const{
+    mint res(1),x(*this);
+    if (n<0){
+      n = -n;
+      x =(*this).inv();
+    }
+    while(n){
+      if(n&1)res*=x;
+      x*=x;
+      n>>=1;
+    }
+    return res;
+  }
+  mint inv()const{return pow(mod-2);}
+  /*mint inv()const{
+    int x,y;
+    int g=extgcd(v,mod,x,y);
+    assert(g==1);
+    if(x<0)x+=mod;
+    return mint(x);
+    }*/
+  friend ostream& operator<<(ostream&os,const mint&m){
+    return os<<m.x;
+  }
+  bool operator<(const mint&r)const{return x<r.x;}
+  bool operator==(const mint&r)const{return x==r.x;}
+};
+//}}}
