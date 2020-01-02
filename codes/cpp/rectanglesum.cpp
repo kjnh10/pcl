0abf38a1d612db574c3dddaa1c29b43e109f56a4
kjnh10 <kojinho10@gmail.com>
2020-01-02 21:07:22 +0900

    Add rectanglesum to snippet

--- codes/cpp/rectanglesum.cpp
+++ codes/cpp/rectanglesum.cpp
@@ -1,17 +1,20 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//%snippet.set('RectangleSum')%
+
 template <typename T>
 struct RectangleSum {
   vector<vector<T>> sum;
-  T GetSum(int left, int right, int top, int bottom) { //[left, right], [top, bottom]
+  T GetSum(int left, int right, int top, int bottom) { //[left, right], [top, bottom]{{{
     T res = sum[bottom][right];
     if (left > 0) res -= sum[bottom][left - 1];
     if (top > 0) res -= sum[top - 1][right];
     if (left > 0 && top > 0) res += sum[top - 1][left - 1];
     return res;
-  }
-  void init(const vector<vector<T>> &s, int h, int w) {
+  }/*}}}*/
+
+  void init(const vector<vector<T>> &s, int h, int w) {/*{{{*/
     sum.resize(h);
     for (int i = 0; i < h; i ++) sum[i].resize(w, 0);
     for (int y = 0; y < h; y ++) {
@@ -22,5 +25,7 @@ struct RectangleSum {
         if (y > 0 && x > 0) sum[y][x] -= sum[y - 1][x - 1];
       }
     }
-  }
+  }/*}}}*/
 };
+
+//%snippet.end()%
