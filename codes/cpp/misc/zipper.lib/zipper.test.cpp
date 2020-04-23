97f72fd0b39ae3e4e055ceb976f887dc7e146b62
kjnh10 <kojinho10@gmail.com>
2020-04-23 18:58:05 +0900

    add simple test for zipper.hpp

---
+++ codes/cpp/misc/zipper.lib/zipper.test.cpp
@@ -0,0 +1,26 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
+#include "zipper.hpp"
+
+signed main(){
+    vector<int> x({3,10,5,(int)1e12});
+    auto z1 = zipper(x); // x: vector<long long>;
+    dump(z1);
+    auto z2 = zipper(x, INF+5);
+    dump(z2);
+
+    auto z3 = zipper();
+    z3.add_value(3);
+    z3.add_value(5);
+    z3.add_value(10);
+    z3.add_value((int)1e12);
+    z3.build();
+    dump(z3);
+
+    dump(z1(5));
+    assert(z1(5)==2);
+    assert(z1.unzip(4)==(int)1e12);
+    dump(z1.get_zipped(x));
+    assert(z1.get_zipped(x) == vector<int>({1,3,2,4}));
+
+    return 0;
+}
