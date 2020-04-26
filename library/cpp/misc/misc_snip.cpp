ef588f481b0baf71a0ff9405d3eaac969e6e60ee
kjnh10 <kojinho10@gmail.com>
2020-04-26 14:31:07 +0900

    fix indent of snippet

--- library/cpp/misc/misc_snip.cpp
+++ library/cpp/misc/misc_snip.cpp
@@ -5,31 +5,31 @@ cout << ${0:TARGET} << endl;
 rep(i, sz(${1:x})) cout << $1[i] << (i!=sz($1)-1 ? " " : "\n");
 
 //%snippet.set('?')%
-  (${1} ? ${2} : ${3})
+(${1} ? ${2} : ${3})
 
 //%snippet.set('inv')%
 vector<int> ${1}(n);
 rep(i, n) {
-  cin>>$1[i];
+    cin>>$1[i];
 }
 dump($1);${0}
 
 //%snippet.set('invv')%
 vector<vector<int>> ${1}(h, vector<int>(w));
 rep(i, h){
-  rep(j,w){
-    cin>>$1[i][j];
-  }
+    rep(j,w){
+        cin>>$1[i][j];
+    }
 }
 
 //%snippet.set('invvs')%
 vector<vector<int>> ${1}(h, vector<int>(w));
 rep(i, h){
-  string tmp;cin>>tmp;
-  rep(j,w){
-    if (tmp[j]=='#') $1[i][j]=1;
-    else             $1[i][j]=0; 
-  }
+    string tmp;cin>>tmp;
+    rep(j,w){
+        if (tmp[j]=='#') $1[i][j]=1;
+        else             $1[i][j]=0; 
+    }
 }
 
 //%snippet.set('rep')%
@@ -86,10 +86,10 @@ do{
 //%snippet.set('to_bin')%
 #define N 61
 string to_bin(int n, int d){
-  // nをd桁の2進数で表示する。
-  stringstream ss;
-  ss << bitset<N>(n);
-  return ss.str().substr(N-d, d);
+    // nをd桁の2進数で表示する。
+    stringstream ss;
+    ss << bitset<N>(n);
+    return ss.str().substr(N-d, d);
 }
 // int d = (32 - __builtin_clz(n));  // 最大桁のd: (2^d)
 // int f = N - d; to_bin()で返ってきた文字列で最初に1が立っているindex
@@ -98,7 +98,7 @@ string to_bin(int n, int d){
 // init
 vector<vector<int>> d(n, vector<int>(n, INF));
 rep(i, n)rep(j, n){
-  if (i==j) d[i][j] = 0;
+    if (i==j) d[i][j] = 0;
 }
 
 // input
@@ -106,11 +106,11 @@ ${0};
 
 // calc
 rep(k, n){
-  rep(i, n){
-    rep(j, n){
-      chmin(d[i][j], d[i][k]+d[k][j]);
+    rep(i, n){
+        rep(j, n){
+            chmin(d[i][j], d[i][k]+d[k][j]);
+        }
     }
-  }
 }
 dump_2d(d, n, n);
 
@@ -120,22 +120,22 @@ int n = sz(s);
 vector<vector<int>> dp(2, vector<int>(n));
 dp[0][0] = 1;
 rep(i, 1, n+1){  //桁数
-  rep(leq, 2){  //未満確定フラグ
-    int lim = leq ? 9 : s[i-1]-'0';
-    rep(d, lim+1){
-      dp[leq || d<lim][i] += dp[leq][i-1];
+    rep(leq, 2){  //未満確定フラグ
+        int lim = leq ? 9 : s[i-1]-'0';
+        rep(d, lim+1){
+            dp[leq || d<lim][i] += dp[leq][i-1];
+        }
     }
-  }
 }
 
 
 //%snippet.set('rangesum')%
 int rangesum(int l, int r){
-  // a[]: accumulative sum
-  l = max(0, l);
-  // r = min(0, n-1);
-  if (l > r) return 0;
-  return a[r] - (l-1>=0 ? a[l-1] : 0);
+    // a[]: accumulative sum
+    l = max(0, l);
+    // r = min(0, n-1);
+    if (l > r) return 0;
+    return a[r] - (l-1>=0 ? a[l-1] : 0);
 }
 
 
@@ -143,41 +143,31 @@ int rangesum(int l, int r){
 // acumulative sum vector {{{
 template<typename T>
 struct asvector {
-  vector<T> cum; //accumulative sum
-  asvector(vector<T> a): cum(a){  // {{{  // constructor
-    rep(i, 1, sz(a))  cum[i]+=cum[i-1];
-  } // }}}
+    vector<T> cum; //accumulative sum
+    asvector(vector<T> a): cum(a){  // {{{  // constructor
+        rep(i, 1, sz(a))  cum[i]+=cum[i-1];
+    } // }}}
 
-  T sum(int l, int r) {  // return sum of [l, r) of data.  {{{
-    l = max(0LL, l);
-    r = min(r, sz(cum));
-    if (l<r){
-      return cum[r-1] - (l-1>=0 ? cum[l-1] : 0);
-    }
-    else{
-      return 0;
-    }
-  } // }}}
+    T sum(int l, int r) {  // return sum of [l, r) of data.  {{{
+        l = max(0LL, l);
+        r = min(r, sz(cum));
+        if (l<r){
+            return cum[r-1] - (l-1>=0 ? cum[l-1] : 0);
+        }
+        else{
+            return 0;
+        }
+    } // }}}
 
-  T& operator[](int i) { // {{{
-    return cum[i]; 
-  } // }}}
+    T& operator[](int i) { // {{{
+        return cum[i]; 
+    } // }}}
 
-  friend ostream& operator<<(ostream &os, asvector<T>& as){//{{{
-    os << as.cum; return os;
-  } //}}}
+    friend ostream& operator<<(ostream &os, asvector<T>& as){//{{{
+        os << as.cum; return os;
+    } //}}}
 };
 //}}}
-
-
-//%snippet.set('sandbox')%
-using namespace std;
-#include <bits/stdc++.h>
-#include <iostream>
-
-int main(){
-  return 0;
-}
 
 
 //%snippet.set('remove_leading_zeros')%
@@ -188,21 +178,21 @@ operator//%snippet.set('pow2')%
 int p[500001];  // power of 2 {{{
 p[0] = 1;
 rep(i, 1, 500002){
-  p[i] = p[i-1]*2;
-  p[i] %= MOD;
+    p[i] = p[i-1]*2;
+    p[i] %= MOD;
 }
 // }}}
 
 //%snippet.set('pow')%
 int pow_(int b, int x){ //{{{
-  if (x==1) return b%M;
-  if (x%2==0){
-    int t = pow_(b, x/2);
-    return (t*t)%M;
-  }
-  else{
-    return pow_(b, x-1)*b%M;
-  }
+    if (x==1) return b%M;
+    if (x%2==0){
+        int t = pow_(b, x/2);
+        return (t*t)%M;
+    }
+    else{
+        return pow_(b, x-1)*b%M;
+    }
 } //}}}
 
 
@@ -215,11 +205,11 @@ auto f = [&](){
 //%snippet.set('dfs_lambda')%
 vector<int> used(n);
 auto dfs = [&](const auto& dfs, int u) -> void {
-  used[u] = 1;
-  each(e, g[u]){
-    if (used[e.to]) continue;
-    dfs(dfs, e.to);
-  }
+    used[u] = 1;
+    each(e, g[u]){
+        if (used[e.to]) continue;
+        dfs(dfs, e.to);
+    }
 };
 dfs(dfs, 0);
 
@@ -232,47 +222,47 @@ int dy[]={0, 0, 1, -1};
 //%snippet.set('ostream')%
 //%snippet.config({'alias':'<<'})%
 ostream& operator<<(ostream& os, const ${1:type}& ${2}){
-  os << ${3};
-  return os;
+    os << ${3};
+    return os;
 }
 
 //%snippet.set('query')%
 int query(){
-  cerrendl;
+    cerrendl;
 
-  return 0;
+    return 0;
 }
 
 signed main(){
-  int Q;cin>>Q;
-  rep(_, Q){
-    query();
-  }
-  return 0;
+    int Q;cin>>Q;
+    rep(_, Q){
+        query();
+    }
+    return 0;
 }
 
 
 //%snippet.set('random_generator')%
 uint64_t rng() {
-  static mt19937 x(chrono::steady_clock::now().time_since_epoch().count());
-  return uniform_int_distribution<uint64_t>(0, -1)(x);
+    static mt19937 x(chrono::steady_clock::now().time_since_epoch().count());
+    return uniform_int_distribution<uint64_t>(0, -1)(x);
 }
 
 
 //%snippet.set('bitall')%
 rep(mask, 1<<n){
-  vi vmask(n);
-  rep(i, n){
-    if (mask&(1<<i)) vmask[i] = 1;
-  }
-  cerrendl;
-  dump(vmask);
-  rep(i, n){
-    if (vmask[i]){
+    vi vmask(n);
+    rep(i, n){
+        if (mask&(1<<i)) vmask[i] = 1;
     }
-    else{
+    cerrendl;
+    dump(vmask);
+    rep(i, n){
+        if (vmask[i]){
+        }
+        else{
+        }
     }
-  }
 }
 
 
@@ -294,7 +284,7 @@ int query(int u, int v){/*{{{*/
 
     cout << "?" << " " << u << " " << v << endl;
     fflush(stdout);
-    
+
     // int res = tr.lca(u, v);
     int res; cin>>res;
 
