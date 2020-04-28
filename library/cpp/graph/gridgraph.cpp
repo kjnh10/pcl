f117ea2bf039f643f926c30c8bf0b59bbb9ba63b
kjnh10 <kojinho10@gmail.com>
2020-04-29 03:54:04 +0900

    gridgrapのtemplateをgraph.hppに対応した形に変更

--- library/cpp/graph/gridgraph.cpp
+++ library/cpp/graph/gridgraph.cpp
@@ -1,43 +1,54 @@
 #include "../header.hpp"
+#include "graph.hpp"
 
 signed main() {
     //%snippet.set('gridgraph')%
 
     // grid graphを通常のグラフに格納する。
-    // (i, j) -> i*w + j
-    // u -> (u/w, u%w)
-
     int h, w;
     cin >> h >> w;
-    int n = h * w;
+    int n = h * w;  // 頂点数
     vvi block(h, vi(w));
     rep(i, h) {
         string s;
         cin >> s;
-        rep(j, w) { block[i][j] = (s[j] == '.' ? 0 : 1); }
+        rep(j, w) { block[i][j] = (s[j] == '#' ? 1 : 0); }
     }
 
-    vvi g(n);
-    // Graph G;
+    Graph g(n);
+
+    auto nid = [&](int i, int j){return (i*w + j);};
+    auto pos = [&](int u) -> pair<int, int> { return {u/w, u%w}; };
+    // int u = nid(i, j);
+    // auto [i,j] = pos(u);
 
     int di[] = {1, -1, 0, 0};
     int dj[] = {0, 0, 1, -1};
+
+    // 下と右のみ. rep(k, 4)をrep(k, 2)に変更するのも忘れない。
+    // int di[] = {1, 0};
+    // int dj[] = {0, 1};
     rep(i, h) rep(j, w) {
         if (block[i][j]) continue;  // blockから出る辺はない
-        int u = i * w + j;
-
         rep(k, 4) {
             int ni = i + di[k];
             int nj = j + dj[k];
-            int v = ni * w + nj;
             if (is_in(ni, 0, h) && is_in(nj, 0, w)) {
                 if (block[ni][nj]) continue;  // blockに入る辺はない
-                g[u].pb(v);
-                // G.add_edge(u, v, 1);
+                g.add_edge(nid(i, j), nid(ni, nj));
                 // 自分から生える辺だけでよい。そうしないと二重辺になってしまう。
             }
         }
     }
+
+    // https://atcoder.jp/contests/abc151/submissions/12467532
+    // (language updateが終わるまでは未veriyf)
+    int ans = 0;
+    rep(s, n){
+        auto d = g.dijkstra(s);
+        rep(i, sz(d)) if (d[i]<INF) chmax(ans, d[i]);
+    }
+    cout << ans << endl;
 
     //%snippet.end()%
 }
