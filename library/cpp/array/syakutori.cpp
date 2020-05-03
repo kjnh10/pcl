4d78f8b4e3fec25dd0cb353967625a8656976ff5
kjnh10 <kojinho10@gmail.com>
2020-05-03 20:49:22 +0900

    updte syakutori

--- library/cpp/array/syakutori.cpp
+++ library/cpp/array/syakutori.cpp
@@ -1,27 +1,36 @@
+#include "../header.hpp"
+
+vector<int> x;
+int k;
+int n;
+
+signed main(){
 //%snippet.set('syakutori')%
 //%snippet.config({'abbr':'syakutori-hou-[l,r)', 'indent':True})%
-int res = 0;
-int sum = 0;
+    int sum = 0;
 
-auto isok = [&](int r) {  // [l, r] e.g [l, r+1) is ok?
-    if (sum + s[r - 1] <= k)
-        return true;
-    else
-        return false;
-};
+    auto isok = [&](int next_r) {  // check [l, next_r) is ok?
+        if (sum + x[next_r - 1] <= k)
+            return true;
+        else
+            return false;
+    };
 
-int r = 0;
-for (int l = 0; l < n; ++l) {
-    while (r < n && isok(r + 1)) {
-        sum += s[r];
-        ++r;
+    int res = 0;
+    int r = 0;
+    for (int l = 0; l < n; ++l) {
+        // [l, r)を限界まで広げる。
+        while (r < n && isok(r + 1)) {
+            sum += x[r];
+            ++r;
+        }
+        chmax(res, (r - l));
+
+        // leftをincrementする準備
+        if (r == l)
+            ++r;  // 空区間から空区間への変動なので何もしない。
+        else
+            sum -= x[l];
     }
-    chmax(res, (r - l));
-
-    // leftをincrementする準備
-    if (r == l)
-        ++r;  // l,rが同時に動くのでsumは変動しない。
-    else
-        sum -= s[l];
-}
 //%snippet.end()%
+}
