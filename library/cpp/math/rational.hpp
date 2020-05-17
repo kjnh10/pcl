41dcbb87b941855efb7342523feece8efa02d200
kjnh10 <kojinho10@gmail.com>
2020-05-18 02:55:21 +0900

    update rational to struct

--- library/cpp/math/rational.hpp
+++ library/cpp/math/rational.hpp
@@ -1,23 +1,22 @@
 #include "../header.hpp"
+#include "geometry/p2.hpp"
 
 //%snippet.set('rational')%
 //%snippet.config({'alias':'fraction'})%
 //%snippet.fold()%
 
 template<class T=ll> 
-pair<T, T> rational(T x, T y){
-    // 有理数をユニークな表現にして返す。
-    // 0 <= theta < 180で返しているイメージ
-    T g = gcd(x, y);
-    x /= g; y /= g;
-    if (y<0) {
-        x *= -1; y *= -1;
+struct rational : public P2<T> {
+    rational(){}
+    rational(T _x, T _y) : P2<T>(_x, _y) {
+        T g = gcd(this->x, this->y);
+        this->x /= g; this->y /= g;
+        if (this->y<0 or (this->y==0 && this->x==-1)) this->x *= -1, this->y *= -1;
     }
-    else if (y==0 && x==-1){
-        x *= -1; y *= -1;
-    }
-    return {x, y};
-}
+};
+// rational(3, 5);
+// assert(rational(3, 5) == rational(-6, -10));
+// map<rational<ll>, int> cnt;  // keyにもできる。
 
 //%snippet.end()%
 
