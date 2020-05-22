35b0ad8319872346feced7d52e036098545c14d8
kjnh10 <kojinho10@gmail.com>
2020-05-22 15:24:29 +0900

    update

--- library/cpp/graph/unionfind_with_potential.hpp
+++ library/cpp/graph/unionfind_with_potential.hpp
@@ -1,3 +1,4 @@
+#include "../header.hpp"
 
 //%snippet.set('unionfind_with_potential')%
 //%snippet.fold()%
@@ -7,12 +8,10 @@ struct UnionFind {
     vector<int> par;   // par[x]: parent of x. if root, -size.
     int gcount;         // count of groups
     vector<Pot>  diff;
-
     UnionFind() {}
     UnionFind(int _n, Pot SUM_UNITY = 0) : par(_n, -1), gcount(_n), diff(_n, SUM_UNITY) {}
-
-    bool merge(int x, int y, Pot d) { 
-        // d:= y - x
+    bool merge(int x, int y, Pot d) { /*{{{*/
+        // d:= wight(y) - weight(x)
         int rx = root(x);
         int ry = root(y);
         if (rx != ry) {
@@ -28,10 +27,14 @@ struct UnionFind {
             }
             gcount--;
         }
+        else{
+            if (weight(y) + (-weight(x)) != d){
+                throw runtime_error("contradiction exist");
+            }
+        }
         return rx != ry;
-    } 
-
-    int root(int x) {
+    } /*}}}*/
+    int root(int x) {/*{{{*/
         if (is_root(x)){
             return x;
         }
@@ -41,17 +44,23 @@ struct UnionFind {
             par[x] = r;
             return par[x];
         }
-    }
-
-    Pot weight(int x){
+    }/*}}}*/
+    Pot weight(int x){/*{{{*/
         root(x); // 経路圧縮
         return diff[x];
-    }
-
+    }/*}}}*/
+    Pot diff(int x, int y){/*{{{*/
+        if (!same(x, y)){
+            string s = to_string(x) + " and " + to_string(y) + " are not in the same group.";
+            throw invalid_argument(s);
+        }
+        else{
+            return weight(y) + (-weight(x));
+        }
+    }/*}}}*/
     bool is_root(int x) { return par[x] < 0; }
     bool same(int x, int y) { return root(x) == root(y); }
     int size(int x) { return -par[root(x)]; }
-
 #if defined(PCM) || defined(LOCAL)  // {{{
     friend ostream& operator<<(ostream& os, UnionFind& uf) {
         map<int, vector<int>> group;
