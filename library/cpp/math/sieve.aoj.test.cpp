dd35f52a4a520bafd7f4e684bf8eb44886b38eac
kjnh10 <kojinho10@gmail.com>
2020-06-01 23:57:45 +0900

    add test for sieve

---
+++ library/cpp/math/sieve.aoj.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=NTL_1_A&lang=ja"
+
+#include "sieve.hpp"
+
+int main(){
+    ll n;cin>>n;
+    auto ps = sv.prime_factor(n);
+
+    vl ans;
+    each(el, ps){
+        auto [p, c] = el;
+        rep(_, c) ans.pb(p);
+    }
+
+    cout << n << ": ";
+    rep(i, sz(ans)) cout << ans[i] << (i!=sz(ans)-1 ? " " : "\n");
+}
---
+++ library/cpp/math/sieve.test.cpp
@@ -0,0 +1,41 @@
+#define PROBLEM "http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
+#include "sieve.hpp"
+
+bool is_prime_naive(ll n) { /*{{{*/
+    for (ll i = 2; i * i <= n; i++) {
+        if (n % i == 0) return false;
+    }
+    return n != 1;
+} /*}}}*/
+
+uint64_t rng() {
+    static mt19937 x(chrono::steady_clock::now().time_since_epoch().count());
+    return uniform_int_distribution<uint64_t>(0, sv.n * 2)(x);
+}
+
+void test(ll m){
+    // dump(m);
+
+    auto ps = sv.prime_factor(m);
+    // dump(ps);
+
+    ll restore = 1;
+    each(p, ps){
+        if (is_prime_naive(p.first)==false) assert(false);
+        rep(i, p.second) restore *= p.first;
+    }
+    assert(restore == m);
+}
+
+int main(){
+    assert(sz(sv.prime_factor(1))==0);
+    // test(sv.n * sv.n + 1);  // make sure that this will fail
+
+    int num = 100000;
+    while(num--){
+        ll m = rng();
+        test(m);
+    }
+
+    cout << "Hello World" << endl;
+}
