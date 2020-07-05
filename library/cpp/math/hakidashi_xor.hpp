d62b14004bae81290d7b042fd3d301bbf0fbedc9
kjnh10 <kojinho10@gmail.com>
2020-07-05 18:15:51 +0900

    add hakidashi xor

---
+++ library/cpp/math/hakidashi_xor.hpp
@@ -0,0 +1,28 @@
+#include "../header.hpp"
+
+//%snippet.set('hakidasi_xor')%
+vector<ll> hakidasi_xor(vector<ll>& a){
+    int r = 0;
+    int n = sz(a);
+    r_rep(d, 0, 64){
+        int p = -1;
+        rep(i, r, n) {
+            if (a[i]>>d&1) {
+                p = i;
+            }
+        }
+        if (p == -1) continue;
+
+        swap(a[r], a[p]);
+        rep(i, n){
+            if (a[i]>>d&1 && i!=r) a[i] ^= a[r];
+        }
+        r++;
+    }
+    vector<ll> res;
+    rep(i, r) res.pb(a[i]);
+    return res;
+}
+//%snippet.end()%
+
+// verified by https://atcoder.jp/contests/agc045/tasks/agc045_a
