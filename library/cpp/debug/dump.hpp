de4e6b26f03b8b8bd25abd47ed70fd61d3923b3c
kjnh10 <kojinho10@gmail.com>
2020-10-21 16:08:40 +0900

    improve dump_2d format

--- library/cpp/debug/dump.hpp
+++ library/cpp/debug/dump.hpp
@@ -46,26 +46,22 @@ namespace dm {
         dm::varidx.pop();                                                    \
     }
 
-#define dump_1d(x, n)                                                        \
-    {                                                                        \
-        DUMPOUT << "  " << #x << "[" << #n << "]"                            \
-                << ":=> {";                                                  \
-        for(dm::i=0; dm::i<n; ++dm::i)                                       \
-            DUMPOUT << x[dm::i] << (((dum::i) == (n - 1)) ? "}" : ", ");     \
-        DUMPOUT << " in [" << __LINE__ << "]" << endl;                       \
+#define dump_1d(x, n)                                                          \
+    {                                                                          \
+        DUMPOUT << #x << "[" << #n << "]"                              \
+                << ":=> {";                                                    \
+        for(dm::i=0; dm::i<n; ++dm::i)                                         \
+            DUMPOUT << x[dm::i] << (((dum::i) == (n - 1)) ? "}" : ", ");       \
+        DUMPOUT << "  in [" << __LINE__ << ":" << __FUNCTION__ << "]" << endl; \
     }
 
-#define dump_2d(x, n, m)                                                     \
-    {                                                                        \
-        DUMPOUT << "  " << #x << "[" << #n << "]"                            \
-                << "[" << #m << "]"                                          \
-                << ":=> \n";                                                 \
-        for(dm::i=0; dm::i<n; ++dm::i)                                       \
-        for(dm::j=0; dm::j<m; ++dm::j) {                                     \
-            DUMPOUT << ((dm::j == 0) ? "     |" : " ") << x[dm::i][dm::j]    \
-                    << (((dm::j) == (m - 1)) ? "|\n" : " ");                 \
-        }                                                                    \
-        DUMPOUT << "  in [" << __LINE__ << "]" << endl;                      \
+#define dump_2d(x, n, m)                                                       \
+    {                                                                          \
+        DUMPOUT << #x << "[" << #n << "]"                              \
+                << "[" << #m << "]"                                            \
+                << ":=> \n";                                                   \
+        dump_2d_core(x, n, m);                                                 \
+        DUMPOUT << "  in [" << __LINE__ << ":" << __FUNCTION__ << "]" << endl; \
     }
 
 void dump_func() {}
@@ -80,6 +76,60 @@ void dump_func(Head&& head, Tail&&... tail) {
     }
     ++dm::varidx.top();
     dump_func(std::move(tail)...);
+}
+
+template <class T>
+void print_formatted_int(T v, T inf, int len){
+    if (v == inf) {
+        DUMPOUT << string(max(len-2, 0), ' ');
+        DUMPOUT << "∞ ";
+    }
+    else if (v == -inf){
+        DUMPOUT << string(max(len-3, 0), ' ');
+        DUMPOUT << "-∞ ";
+    }
+    else{
+        DUMPOUT << setw(len);
+        DUMPOUT << v;
+    }
+}
+
+template <class T>
+void dump_2d_core(const vector<vector<T>>& x, int n, int m){
+    T inf = numeric_limits<T>::max() / 2.1;
+
+    vector<int> column_len(m, 2);
+    for(int i = 0; i < n ; ++i) for(int j = 0; j < m; ++j) {
+        int len = to_string(x[i][j]).size();
+        if (x[i][j] == inf) len = 2;
+        if (x[i][j] == -inf) len = 3;
+        if (len > column_len[j]) column_len[j] = len;
+    }
+
+    // print header
+    int header_len = 0;
+    for(int j = 0; j < m ; ++j) {
+        if (j == 0) {
+            DUMPOUT << string(7, ' ');
+        }
+        else{
+            DUMPOUT << " ";
+            header_len++;
+        }
+        DUMPOUT << setw(column_len[j]) << j;
+        header_len += column_len[j];
+        DUMPOUT << (((j) == (m - 1)) ? "\n" : "");
+    }
+    DUMPOUT << string(7, ' ');
+    DUMPOUT << string(header_len, '-');
+    DUMPOUT << "\n";
+
+    for(int i = 0; i < n ; ++i) for(int j = 0; j < m; ++j) {
+        if (j == 0) DUMPOUT << setw(5) << i << " |";
+        else DUMPOUT << " ";
+        print_formatted_int(x[i][j], inf, column_len[j]);
+        DUMPOUT << (((j) == (m - 1)) ? "|\n" : "");
+    }
 }
 
 #include "prettyprint.hpp"
