ec2e95c106fcdeb3116e46d49d41733a0042c8aa
kjnh10 <kojinho10@gmail.com>
2020-11-11 23:25:41 +0900

    add int128

---
+++ library/cpp/misc/int128.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include "../../header.hpp"
+
+//%snippet.set('int128')%
+//%snippet.config({'alias':'128'})%
+//%snippet.fold()%
+
+using LL = __int128;
+istream& operator>>(istream& is, LL& v)
+{
+    string s;
+    is >> s;
+    v = 0;
+    for (int i = 0; i < (int)s.size(); i++) {
+        if (isdigit(s[i])) { v = v * 10 + s[i] - '0'; }
+    }
+    if (s[0] == '-') { v *= -1; }
+    return is;
+}
+ 
+ostream& operator<<(ostream& os, const LL& v)
+{
+    if (v == 0) { return (os << "0"); }
+    LL num = v;
+    if (v < 0) {
+        os << '-';
+        num = -num;
+    }
+    string s;
+    for (; num > 0; num /= 10) { s.push_back((char)(num % 10) + '0'); }
+    reverse(s.begin(), s.end());
+    return (os << s);
+}
+
+//%snippet.end()%
