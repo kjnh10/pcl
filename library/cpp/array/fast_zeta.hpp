359979ae319e346de79d1c7765a0523c967eb225
kjnh10 <kojinho10@gmail.com>
2021-08-09 13:06:23 +0900

    refactor zeta

--- library/cpp/array/fast_zeta.hpp
+++ library/cpp/array/fast_zeta.hpp
@@ -5,18 +5,9 @@
 //%snippet.fold()%
 
 template <class T> 
-vec<T> zeta(const vec<T>& a, int n){
-    vec<T> res(a);
-    // 自由度0
-    rep(j, 0, n){
-        // j桁目を自由にする。
-        rep(i, 1<<n) {
-            if (i >> j & 1) {
-                res[i] += res[i ^ 1<<j];
-                // j桁目が立ってないやつからj桁目が立ってるやつに更新が行くので順番の心配は不要
-            }
-        }
-    }
+vector<T> zeta(const vector<T>& a, int d){
+    vector<T> res(a);
+    rep(j, 0, d) rep(i, 1<<d) if (i >> j & 1) res[i] += res[i ^ 1<<j];
     return res;
 }
 
