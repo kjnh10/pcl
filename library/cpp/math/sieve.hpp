c04a8c717db7eb976023709a2d95e2a8e17aa70a
kjnh10 <kojinho10@gmail.com>
2021-08-09 13:06:12 +0900

    add calculation of mobius function value to sieve library

--- library/cpp/math/sieve.hpp
+++ library/cpp/math/sieve.hpp
@@ -7,24 +7,30 @@
 
 struct Sieve {/*{{{*/
     // エラトステネスのふるい O(NloglogN)
-    ll n;                              // n: max number for defined f and primes
-    vector<ll> f;                      // [-1, 2, 3, 2, 5, 2, 7, 2, 3, ....]
+    ll n;                              // n: max number for defined minfactor and primes
+    vector<ll> minfactor;              // [-1, 2, 3, 2, 5, 2, 7, 2, 3, ....]
     vector<ll> primes;                 // [2, 3, 5, .......]
-    Sieve(ll n = 1) : n(n), f(n + 1) { /*{{{*/
-        f[0] = f[1] = -1;
-        for (ll i = 2; i <= n; ++i) {
-            if (f[i]) continue;
-            primes.push_back(i);
-            f[i] = i;
-            for (ll j = i * i; j <= n; j += i) {
-                if (!f[j]) f[j] = i;
+    vector<int> mobius;                // メビウス関数値
+
+    Sieve(ll n = 1) : n(n), minfactor(n + 1), mobius(n + 1, 1) { /*{{{*/
+        minfactor[0] = minfactor[1] = -1;
+        for (ll p = 2; p <= n; ++p) {
+            if (minfactor[p]) continue;
+            primes.push_back(p);
+            minfactor[p] = p;
+            mobius[p] = -1;
+            for (ll x = p * p; x <= n; x += p) {
+                if (!minfactor[x]) minfactor[x] = p;
+                if ((x / p) % p == 0) mobius[x] = 0;
+                else mobius[x] *= -1;
             }
         }
     } /*}}}*/
-    bool is_prime(ll x) {
-        if (x <= n) return f[x] == x; 
+
+    bool is_prime(ll x) {/*{{{*/
+        if (x <= n) return minfactor[x] == x; 
         return sz(factor_list(x)) == 1;
-    }
+    }/*}}}*/
 
     vector<ll> factor_list(ll x) { /*{{{*/
         assert(0 < x && x <= n*n); // これが満たされないと正しく計算されない可能性がある。
@@ -32,8 +38,8 @@ struct Sieve {/*{{{*/
         vector<ll> res;
         if (x <= n) {
             while (x != 1) {
-                res.push_back(f[x]);
-                x /= f[x];
+                res.push_back(minfactor[x]);
+                x /= minfactor[x];
             }
         }
         else {
@@ -63,6 +69,25 @@ struct Sieve {/*{{{*/
         }
         return res;  // [(2,1), (3,2), (5,3), .....]
     }                /*}}}*/
+
+    vector<ll> divisors(ll x) { // 高速約数列挙{{{
+        vector<ll> res({1});
+        auto ps = prime_factor(x);
+
+        // 約数列挙
+        for (auto p : ps) {
+            ll s = (ll)res.size();
+            for (ll i = 0; i < s; ++i) {
+                ll v = 1;
+                for (ll j = 0; j < p.second; ++j) {
+                    v *= p.first;
+                    res.push_back(res[i] * v);
+                }
+            }
+        }
+        return res;
+    }/*}}}*/
+
 };/*}}}*/
 Sieve sv(1e6);
 // How to use
